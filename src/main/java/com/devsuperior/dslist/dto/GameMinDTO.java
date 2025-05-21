27a73fc83bff93574105a0496e1f9a7723b6f443
Samyr Tertuliano <samyrtertuliano4@gmail.com>
2025-05-20 22:23:22 -0300

    GameMinDTO, GameRepository, GameService. GameController

---
+++ src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
@@ -0,0 +1,44 @@
+package com.devsuperior.dslist.dto;
+
+
+import com.devsuperior.dslist.entities.Game;
+
+public class GameMinDTO {
+
+    private Long id;
+    private String title;
+    private Integer year;
+    private String imgUrl;
+    private String shortDescription;
+
+    public GameMinDTO() {
+    }
+
+    public GameMinDTO(Game entity) {
+        id = entity.getId();
+        shortDescription = entity.getShortDescription();
+        imgUrl = entity.getImgUrl();
+        year = entity.getYear();
+        title = entity.getTitle();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+}
